8fab9f98ba1f60b7fa2de5b20ff5ed2edff4f2b5
Zeus Olympus <zeusolympusgreekgod@gmail.com>
2025-09-22 02:02:32 +0530

    Debug and fix cycle routes

--- backend/routes/cycleRoutes.js
+++ backend/routes/cycleRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController');
+const protect = require('../middlewares/auth');
+const { startCycle, closeCycle, getCycles } = require('../controllers/cycleController');
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post('/start', protect, startCycle);
+router.post('/close', protect, closeCycle);
+router.get('/', protect, getCycles);
 
 module.exports = router;
